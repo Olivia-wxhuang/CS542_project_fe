23f58093c685c812c068295564c5a9a9084848f1
Tomi392 <tomaschow95@outlook.com>
2019-03-24 14:23:16 -0400

    Added templates for the front end

---
+++ Frontend/js/user.js
@@ -0,0 +1,8 @@
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+$("#side-bar-my-account").click(function(e){
+    e.preventDefault();
+
+})
