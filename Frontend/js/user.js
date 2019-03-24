e2c722e6c5561d484924c155cacd958b2702f03b
Tomi392 <tomaschow95@outlook.com>
2019-03-24 15:48:02 -0400

    Updated user.html

--- Frontend/js/user.js
+++ Frontend/js/user.js
@@ -1,8 +1,29 @@
-$("#menu-toggle").click(function(e) {
+$("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
-$("#side-bar-my-account").click(function(e){
-    e.preventDefault();
+$("#side-bar-my-account").click(function () {
+    let x = document.getElementById("account-details-content");
+    let y = document.getElementById("list-container");
+    console.log('Showing account details');
+    x.style.display = "block";
+    y.style.display = "none";
+});
+$("#side-bar-my-listings").click(function () {
+    let y = document.getElementById("account-details-content");
+    let x = document.getElementById("list-container");
+    console.log('Showing list container');
+    x.style.display = "block";
+    y.style.display = "none";
+});
+$("#side-bar-my-watchlist").click(function () {
+    let y = document.getElementById("account-details-content");
+    let x = document.getElementById("list-container");
+    console.log('Showing list container');
+    x.style.display = "block";
+    y.style.display = "none";
+});
 
-})
+
+
+
