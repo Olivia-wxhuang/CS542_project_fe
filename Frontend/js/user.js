5a3de31d139fcebebdc34f8c98df946d48e60233
Tomi392 <tomaschow95@outlook.com>
2019-03-24 17:30:16 -0400

    Updated addcar.html

--- Frontend/js/user.js
+++ Frontend/js/user.js
@@ -6,17 +6,19 @@ $("#side-bar-my-account").click(function () {
     document.getElementById("account-details-content").style.display = "block";
     document.getElementById("list-container").style.display = "none";
     document.getElementById("watchlist-container").style.display = "none";
+    return false;
 });
 $("#side-bar-my-listings").click(function () {
     document.getElementById("list-container").style.display = "block";
     document.getElementById("watchlist-container").style.display = "none";
     document.getElementById("account-details-content").style.display = "none";
+    return false;
 });
 $("#side-bar-my-watchlist").click(function () {
-    console.log('Showing list container');
     document.getElementById("watchlist-container").style.display = "block";
     document.getElementById("list-container").style.display = "none";
     document.getElementById("account-details-content").style.display = "none";
+    return false;
 });
 
 
