4b5f7967f6ac6c153271d707ebf6759edfc51b4a
kshu1991 <kshu1991@gmail.com>
2019-03-23 21:34:50 -0400

    add model and add car, fix bugs in add user

--- CS542_Backend/src/main/java/com/amazonaws/lambda/demo/AddCarHandler.java
+++ CS542_Backend/src/main/java/com/amazonaws/lambda/demo/AddCarHandler.java
@@ -1,18 +1,23 @@
 package com.amazonaws.lambda.demo;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-import org.json.*;
+import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
-public class AddUserHandler implements RequestStreamHandler {
+public class AddCarHandler implements RequestStreamHandler {
 	JSONParser parser = new JSONParser();
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
@@ -24,25 +29,41 @@ public class AddUserHandler implements RequestStreamHandler {
         String responseCode = "200";
         
         //	default value
+        String VIN = "";
+        int model = -1;
+        int mileage = -1;
+        int price = -1;
+        String color = "";
         String user_name = "";
-        String password = "";
         
         try {
         	JSONObject event = (JSONObject)parser.parse(reader);
         	logger.log(event.toString());
+            if ( event.get("VIN") != null) {
+            	VIN = (String)event.get("VIN");
+            }
+            if ( event.get("model") != null) {
+                model = Integer.parseInt((String)event.get("model"));
+            }
+            if ( event.get("mileage") != null) {
+                mileage = Integer.parseInt((String)event.get("mileage"));
+            }
+            if ( event.get("price") != null) {
+                price = Integer.parseInt((String)event.get("price"));
+            }
+            if ( event.get("color") != null) {
+                color = (String)event.get("color");
+            }
             if ( event.get("user_name") != null) {
                 user_name = (String)event.get("user_name");
             }
-            if ( event.get("password") != null) {
-            	password = (String)event.get("password");
-            }
 
             //	Error handle
-            if (user_name == "" || password == "") {
-            	throw new Exception("Invalid input to add new user");
+            if (VIN == "" || model == -1 || mileage == -1 || color == "" || user_name == "") {
+            	throw new Exception("Invalid input to add new car");
             }
             
-            addUser(user_name, password, context);
+            addCar(VIN, model, mileage, price, color, user_name, context);
             
             JSONObject responseBody = new JSONObject();
             responseBody.put("input", event.toString());
@@ -60,8 +81,8 @@ public class AddUserHandler implements RequestStreamHandler {
         writer.write(responseJson.toString());  
         writer.close();
     }
-    
-	private void addUser(String user_name, String password, Context context) {
+	private void addCar(String VIN, int model, int mileage, int price, String color, String user_name,
+			Context context) {
 		LambdaLogger logger = context.getLogger();
 		try {
     		String url = "jdbc:mysql://cardb.clnm8zsvchg3.us-east-2.rds.amazonaws.com:3306";
@@ -71,10 +92,11 @@ public class AddUserHandler implements RequestStreamHandler {
     	    Connection conn = DriverManager.getConnection(url, username, dbpassword);
     	    Statement stmt = conn.createStatement();
     	    
-    	    //	Add new user
-    	    String newUser = String.format("INSERT INTO innodb.User (user_name, password) VALUES ('%s', '%s')",
-    	    		user_name, password);
-    	    stmt.executeUpdate(newUser);
+    	    //	Add new car
+    	    String newCar = String.format("INSERT INTO innodb.Car (VIN, model, mileage, price, color, user_name)"
+    	    		+ " VALUES ('%s', '%s', %d, %d, '%s', '%s')",
+    	    		VIN, model, mileage, price, color, user_name);
+    	    stmt.executeUpdate(newCar);
     	    
     	    stmt.close();
     	    conn.close();
--- CS542_Backend/src/main/java/com/amazonaws/lambda/demo/AddModelHandler.java
+++ CS542_Backend/src/main/java/com/amazonaws/lambda/demo/AddModelHandler.java
@@ -1,18 +1,23 @@
 package com.amazonaws.lambda.demo;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-import org.json.*;
+import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
-public class AddUserHandler implements RequestStreamHandler {
+public class AddModelHandler implements RequestStreamHandler {
 	JSONParser parser = new JSONParser();
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
@@ -24,25 +29,45 @@ public class AddUserHandler implements RequestStreamHandler {
         String responseCode = "200";
         
         //	default value
-        String user_name = "";
-        String password = "";
+        String year = "";
+        String make = "";
+        String model = "";
+        String trim = "";
+        String transmission = "";
+        String engine = "";
+        String drive_type = "";
         
         try {
         	JSONObject event = (JSONObject)parser.parse(reader);
         	logger.log(event.toString());
-            if ( event.get("user_name") != null) {
-                user_name = (String)event.get("user_name");
+            if ( event.get("year") != null) {
+                year = (String)event.get("year");
             }
-            if ( event.get("password") != null) {
-            	password = (String)event.get("password");
+            if ( event.get("make") != null) {
+                make = (String)event.get("make");
+            }
+            if ( event.get("model") != null) {
+                model = (String)event.get("model");
+            }
+            if ( event.get("trim") != null) {
+                trim = (String)event.get("trim");
+            }
+            if ( event.get("transmission") != null) {
+                transmission = (String)event.get("transmission");
+            }
+            if ( event.get("engine") != null) {
+                engine = (String)event.get("engine");
+            }
+            if ( event.get("drive_type") != null) {
+                drive_type = (String)event.get("drive_type");
             }
 
             //	Error handle
-            if (user_name == "" || password == "") {
-            	throw new Exception("Invalid input to add new user");
+            if (year == "" || make == "" || model == "") {
+            	throw new Exception("Invalid input to add new model");
             }
             
-            addUser(user_name, password, context);
+            addModel(year, make, model, trim, transmission, engine, drive_type, context);
             
             JSONObject responseBody = new JSONObject();
             responseBody.put("input", event.toString());
@@ -60,8 +85,8 @@ public class AddUserHandler implements RequestStreamHandler {
         writer.write(responseJson.toString());  
         writer.close();
     }
-    
-	private void addUser(String user_name, String password, Context context) {
+	private void addModel(String year, String make, String model, String trim, String transmission, String engine,
+			String drive_type, Context context) {
 		LambdaLogger logger = context.getLogger();
 		try {
     		String url = "jdbc:mysql://cardb.clnm8zsvchg3.us-east-2.rds.amazonaws.com:3306";
@@ -71,10 +96,11 @@ public class AddUserHandler implements RequestStreamHandler {
     	    Connection conn = DriverManager.getConnection(url, username, dbpassword);
     	    Statement stmt = conn.createStatement();
     	    
-    	    //	Add new user
-    	    String newUser = String.format("INSERT INTO innodb.User (user_name, password) VALUES ('%s', '%s')",
-    	    		user_name, password);
-    	    stmt.executeUpdate(newUser);
+    	    //	Add new model
+    	    String newModel = String.format("INSERT INTO innodb.Model (year, make, model, trim, transmission, engine, drive_type)"
+    	    		+ " VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')",
+    	    		year, make, model, trim, transmission, engine, drive_type);
+    	    stmt.executeUpdate(newModel);
     	    
     	    stmt.close();
     	    conn.close();
