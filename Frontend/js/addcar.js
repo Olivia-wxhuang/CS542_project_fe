d2d21e0057e6a951f915935de88a1a8e9c5d3b86
Zhou <dzhou@wpi.edu>
2019-04-04 13:44:39 -0400

    Modified frontend pages

--- Frontend/js/addcar.js
+++ Frontend/js/addcar.js
@@ -16,4 +16,9 @@ $("#create-new-listing").click(function (){
     console.log('Validate new listing form...')
     // Do the validation before submission.
     return false;
+});
+$("#next").click(function(){
+	document.getElementById("detailed-info-content").style.display = "block";
+    document.getElementById("basic-info-content").style.display = "none";
+    return false;
 })
